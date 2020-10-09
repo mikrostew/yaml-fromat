a158b9c354696d7f6f2c7339188039ddae9de985
Michael Stewart <mikrostew@gmail.com>
2020-10-09 00:24:19 -0700

    write yaml FM to a string

---
+++ tests/write-string-test.js
@@ -0,0 +1,170 @@
+const writeString = require('../lib/write-string');
+const { expect } = require('chai');
+
+// helpers
+
+function testWriteString(inputString, inputYaml, expectedJson) {
+  return writeString(inputString, inputYaml).then((result) => {
+    expect(result).to.deep.equal(expectedJson);
+  });
+}
+
+function testReadStringErrors(inputString, expectedErrMsg) {
+  return writeString(inputString)
+    .then(() => {
+      // TODO: this is not quite right, as this will hit the catch()
+      throw new Error('Expected this to fail');
+    })
+    .catch((e) => {
+      expect(e).to.include(expectedErrMsg);
+    });
+}
+
+// tests
+
+describe('write-string', () => {
+
+  describe('empty string', () => {
+
+    it('key/value', () => {
+
+      return testWriteString(
+'',
+'foo: bar',
+// note the trailing newline
+`---
+foo: bar
+---
+`
+      );
+    });
+  });
+
+  // TODO
+  // describe('no front matter', () => {
+  //   return testWriteString(
+// 'no front matter here',
+// // TODO: test the contents?
+// {
+  // _contents: 'no front matter here',
+// },
+  //   );
+  // });
+
+  // TODO
+  // it('empty front matter', () => {
+  //   return testWriteString(
+// `---
+// ---`,
+// {
+  // _contents: '',
+// },
+  //   );
+  // });
+
+  // TODO
+  // it('key/value pair', () => {
+  //   return testWriteString(
+// `---
+// foo: bar
+// ---`,
+// {
+  // foo: 'bar',
+  // _contents: '',
+// },
+  //   );
+  // });
+
+  // TODO
+  // it('complicated stuff', () => {
+  //   return testWriteString(
+// `---
+// foo: bar
+// things:
+// - a
+// - b
+// - c
+// thing:
+  // only: one
+// ---`,
+// {
+  // foo: 'bar',
+  // things: [ 'a', 'b', 'c'],
+  // thing: { only: 'one' },
+  // _contents: '',
+// },
+  //   );
+  // });
+
+  // TODO
+  // it('front matter and contents', () => {
+  //   return testWriteString(
+// `---
+// foo: bar
+// thing:
+  // only: one
+// ---
+// Here is some text
+
+// OK`,
+// {
+  // foo: 'bar',
+  // thing: { only: 'one' },
+  // _contents: 'Here is some text\n\nOK',
+// },
+  //   );
+  // });
+
+  // TODO
+  // describe('errors', () => {
+
+  //   it('top level scalar', () => {
+  //     return testReadStringErrors(
+// `---
+// 5
+// ---`,
+// 'Top level should be an object',
+  //     );
+  //   });
+
+  //   it('top level boolean', () => {
+  //     return testReadStringErrors(
+// `---
+// true
+// ---`,
+// 'Top level should be an object',
+  //     );
+  //   });
+
+  //   it('top level array', () => {
+  //     return testReadStringErrors(
+// `---
+// - a
+// - b
+// - c
+// ---`,
+// 'Top level should be an object',
+  //     );
+  //   });
+
+  //   it('top level string', () => {
+  //     return testReadStringErrors(
+// `---
+// what
+// ---`,
+// 'Top level should be an object',
+  //     );
+  //   });
+
+  //   it('malformed yaml', () => {
+  //     return testReadStringErrors(
+// `---
+// oops: [ a, b
+// ---`,
+// 'Error parsing YAML in front matter',
+  //     );
+  //   });
+
+  // });
+
+});
