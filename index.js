e5c522651fb0276cd59943448bff29e2c2930601
Michael Stewart <mikrostew@gmail.com>
2020-11-11 07:55:33 -0800

    basically how writeFile should work

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const readFile = require('./lib/read-file');
 const readString = require('./lib/read-string');
+const writeFile =  require('./lib/write-file');
 const writeString =  require('./lib/write-string');
 
 module.exports = {
   readFile,
   readString,
+  writeFile,
   writeString,
 }
